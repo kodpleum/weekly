96ad74c5d17097c3970cf78bb299bb4be943ca44
KodPleum <sir.tanawat@gmail.com>
2022-08-02 23:21:04 +0700

    week2work

---
+++ week2/findmax2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define max(a, b) ((a > b) ? a : b)
+// จงเขียนโปรแกรมเพื่อรับตัวเลข 3 ตัว แล้วหาตัวเลขที่มีค่ามากที่สุด (Level 1)
+
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d", max(a, max(b, c)));
+    return 0;
+}
